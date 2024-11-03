0de623c0919cff60bab01d6affb30447ff5902e6
ThePolonsky <143210864+ThePolonsky@users.noreply.github.com>
2024-11-03 23:42:24 +0500

    -Get на список файлов -Get на чтение html файла из файловой системы -Post на сохранение нового стола и пивота

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const {Pool} = require('pg');
+const result = require("pg/lib/query");
 
 app.use(cors());
 app.use(express.json());
@@ -12,6 +13,67 @@ const pool = new Pool({
     database: 'postgres',
     password: '0800',
     port: 5432,
+});
+
+//e|---------Столы---------|
+
+//q|---Получение списка столов по ID пользователя---|
+
+app.get('/api/tables/:userId', async (req, res) => {
+    const userId = req.params.userId;
+    try {
+        // const results = await pool.query(`SELECT * FROM users_tables_pivot WHERE user_id = $1`, [userId]);
+        const results = await pool.query(`WITH user_tables AS (
+              SELECT DISTINCT table_id
+              FROM users_tables_pivot
+              WHERE user_id = ${userId}
+            ),
+            table_info AS (
+              SELECT t.id, t.title, t.created_at
+              FROM tables t
+              INNER JOIN user_tables ut ON t.id = ut.table_id
+            )
+            SELECT 
+              ti.id,
+              ti.title,
+              ti.created_at
+            FROM table_info ti
+            ORDER BY ti.id;
+        `);
+        res.json(results.rows);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({message: 'Error fetching tables'});
+    }
+});
+
+//q|---Добавление нового стола---|
+
+const createTable = async (title, userId, res) => {
+    try {
+        const addResults = await pool.query(`INSERT INTO tables (title, created_at) VALUES ($1, NOW()) RETURNING *`, [title]);
+        const tableId = addResults.rows[0].id;
+        const pivotResults = await pool.query(`INSERT INTO users_tables_pivot (user_id, table_id, relation) VALUES ($1, $2, $3) RETURNING *`, [userId, tableId, 'ADMIN']);
+        res.json(addResults.rows[0]);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({message: 'Error creating table'});
+    }
+};
+
+const createUserTablePivot = async (userId, tableId, res) => {
+    try {
+        const results = await pool.query(`INSERT INTO users_tables_pivot (user_id, table_id, relation) VALUES ($1, $2, $3) RETURNING *`, [userId, tableId, 'ADMIN']);
+        res.json(results.rows[0]);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({message: 'Error creating pivot'});
+    }
+};
+
+app.post('/api/tables', async (req, res) => {
+    const {title, userId} = req.body;
+    const tableId = await createTable(title, userId, res);
 });
 
 //q|---Удаление блокнота по ID---|
@@ -29,7 +91,6 @@ app.delete('/api/notebooks/:id', async (req, res) => {
         res.status(500).json({message: 'Error deleting notebook'});
     }
 });
-
 
 //q|---Создание нового блокнота---|
 
@@ -57,35 +118,7 @@ app.get('/api/notebooks/:userId', async (req, res) => {
     }
 });
 
-//q|---Получение списка столов по ID пользователя---|
 
-app.get('/api/tables/:userId', async (req, res) => {
-    const userId = req.params.userId;
-    try {
-        // const results = await pool.query(`SELECT * FROM users_tables_pivot WHERE user_id = $1`, [userId]);
-        const results = await pool.query(`WITH user_tables AS (
-              SELECT DISTINCT table_id
-              FROM users_tables_pivot
-              WHERE user_id = ${userId}
-            ),
-            table_info AS (
-              SELECT tables.id, tables.title, tables.created_at
-              FROM tables tables
-              INNER JOIN user_tables ut ON tables.id = ut.table_id
-            )
-            SELECT 
-              ti.id,
-              ti.title,
-              ti.created_at
-            FROM table_info ti
-            ORDER BY ti.id;
-            `);
-        res.json(results.rows);
-    } catch (err) {
-        console.error(err);
-        res.status(500).json({message: 'Error fetching tables'});
-    }
-});
 
 //q|---Получение списка заметок по ID блокнота---|
 
